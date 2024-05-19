fa05e808e22c4dcfccb3f0a839eaff497b3b02f0
Claraestephan <123934323+Claraestephan@users.noreply.github.com>
2024-05-19 15:45:25 +0300

    Add files via upload

---
+++ ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace I3332.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
